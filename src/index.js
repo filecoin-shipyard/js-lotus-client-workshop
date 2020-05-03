922b129ee535ca8074dff2d6cab80416aa444aba
Glitch (rl1-lotus-workshop) <none>
2020-05-03 17:36:49 +0000

    🏗🏛 Updated with Glitch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import useLotusClient from './use-lotus-client'
+
+function App() {
+  const client = useLotusClient(0, 'node')
+  const [height, setHeight] = useState()
+
+  useEffect(() => {
+    let state = { canceled: false }
+    if (!client) return
+    setHeight('Loading...')
+    async function run() {
+      if (state.canceled) return
+      const result = await client.chainHead()
+      if (state.canceled) return
+      setHeight(result.Height)
+      setTimeout(run, 1000)
+    }
+    run()
+    return () => {
+      state.canceled = true
+    }
+  }, [client])
+
+  return (
+    <div>
+      <h2>Height</h2>
+      <h1>{height}</h1>
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
